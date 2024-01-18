46d02ab41b6d131a376bf0c6fd127830b3a050c3
Rishav Raj <rishavraj471@gmail.com>
2024-01-18 19:18:42 +0530

    social media linkes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import react from 'react';
 import NavBar from './components/NavBar.jsx';
 import Home from './components/Home.jsx';
+import SocialLinks from './components/SocialLinks.jsx';
+
 function App(){
   return(
     <div>
       <NavBar/>
       <Home/>
+
+      <SocialLinks/>
     </div>
   )
 }
